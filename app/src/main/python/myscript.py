e2330b4830d5a614f5e55ce4c1fa64fa0c7daf45
Adil MIr <l191230@lhr.nu.edu.pk>
2023-03-13 18:41:11 +0500

    Prototype

---
+++ app/src/main/python/myscript.py
@@ -0,0 +1,31 @@
+import requests
+import urllib
+import time
+def TextToSign(path,text):
+    #while(1):
+        #print('HYgbygy')
+        #try:
+            response = requests.post("https://bilalsardar-text-to-sign.hf.space/run/predict",
+            json={
+            "data": [
+                text,
+            ]}).json()
+
+            data = response["data"]
+            print('Hssy')
+            url = "https://bilalsardar-text-to-sign.hf.space/file="+data[0]["name"]
+            save_as = path
+
+            data1 = urllib.request.urlopen(url)
+
+            f = open(save_as,'wb')
+            f.write(data1.read())
+            f.close()
+
+            #break
+            return 'done'
+#         except:
+#             print('jghhfhf')
+#             return 'Wrong Input'
+#            # continue
+
